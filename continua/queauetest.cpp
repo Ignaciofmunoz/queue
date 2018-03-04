eeb0e18876d7020455b20b7974e0f9ba60df9a9f
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-03 22:05:20 -0300

    Create queauetest.cpp

---
+++ continua/queauetest.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <Cassert>
+#include "queaue.h"
+#define SIZE 15
+using namespace std;
+
+int main()
+{
+using namespace Queue;
+using::Queue::Nodo; //en esto tengo dudas
+using::Queue::Queue;
+Queue q ;//nota al profesor : aca el compilador me advierte de que la referencia es ambigua
+cout<<"se carga  la cola "<<endl;
+for(int i=0;i<SIZE;i++)
+{
+   Enq(q,i);
+   assert((Deq(q))==i);
+   Enq(q,i);
+}
+CoutQueue(q);
+cout<<"se desencolan todos los elementos "<<endl<<endl;
+for(int i=0;i<SIZE;i++)//probar con 15
+{
+   cout<<"se desencola:\t"<<Deq(q)<<endl;
+}
+assert(IsEmpty(q));
+CoutQueue(q);
+cout<<endl<<"se encolan el 1, 46,126 y 6232"<<endl<<endl;
+Enq(q,1);
+assert((Deq(q)==1));
+Enq(q,1);
+Enq(q,46);
+Enq(q,126);
+assert((Deq(q)==126));
+Enq(q,126);
+Enq(q,6232);
+CoutQueue(q);
+cout<<"se desencola dos(2) veces"<<endl<<endl;
+assert(Deq(q)==1);
+Enq(q,1);
+cout<<"se desencola:\t"<<Deq(q)<<endl;
+cout<<"se desencola:\t"<<Deq(q)<<endl<<endl;
+CoutQueue(q);
+cout<<"se desencola dos(2) veces mas "<<endl<<endl;
+cout<<"se desencola:\t"<<Deq(q)<<endl;
+cout<<"se desencola:\t"<<Deq(q)<<endl<<endl;
+CoutQueue(q);
+assert(IsEmpty(q));
+cout<<"----FIN DEL TEST"<<endl;
+return 0;
+}
+
+
